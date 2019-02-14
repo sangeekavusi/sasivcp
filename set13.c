d938d86f0141ae72fb42b8bce37024387fc0adfb
Sasivcp <47106072+Sasivcp@users.noreply.github.com>
2019-02-14 16:27:09 +0530

    Update set13.c

--- set13.c
+++ set13.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
- 
-int main(void)
- {
- 
-char x;
-printf("\n enter the element:");
-scanf("%c",&x);
-if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
+
+#include<stdio.h>
+int main()
 {
-printf("it is a vowel");
-}
+char a;
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+   {printf("vowel");}
 else
-{
-printf("\n it is a consonant");
+printf("consonant");
 }
- 
-return 0;
