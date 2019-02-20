e3ad98492c809a8ddb750b52930287e2f7377f7b
Sasivcp <47106072+Sasivcp@users.noreply.github.com>
2019-02-20 15:29:17 +0530

    Update set13.c

--- set13.c
+++ set13.c
@@ -1,11 +1,11 @@
-
 #include<stdio.h>
 int main()
 {
-char a;
-scanf("%c",&a);
-if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
-   {printf("vowel");}
+char c;
+scanf("%c",&c);
+if((c=='a')||(c=='A')||(c=='e')||(c=='E')||(c=='i')||(c=='I')||(c=='o')||(c=='O')||(c=='u')||(c=='U'))
+printf("vowel");
 else
-printf("consonant");
+printf("consonent");
+return 0;
 }
