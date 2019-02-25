5c4e4c9ad3594c63b1858aa21b0af32e8c45e3ba
Sasivcp <47106072+Sasivcp@users.noreply.github.com>
2019-02-25 16:34:32 +0530

    Create set25.c

---
+++ set25.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int start,end,i;
+scanf("%d%d",&start,&end);
+for(i=start+1;i<end;i++)
+{
+	if(i%2==0)
+	{
+		printf("%d\n",i);
+    getch();
+	}
+}
+}
